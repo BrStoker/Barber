3a21bf0eb492ba88feba7a61441f74ac7094eb9b
Youriy K <79205111484@yandex.com>
2022-11-04 11:24:51 +0300

    Сверстана страница Our History

--- resources/views/includes/history.blade.php
+++ resources/views/includes/history.blade.php
@@ -1,7 +1,15 @@
-<section class="out-history">
-    <h2 class="out-history__title">
-        <span class="out-history__title-first-item">Our</span>
-        <span class="out-history__title-second-item">History</span>
+<section class="our-history">
+    <div class="our-history-wrapper">
+        <img src="{{asset('images/_13126_1.png')}}" alt="our history" class="our-history-image">
 
-    </h2>
+        <h2 class="our-history__title">
+            <span class="our-history__title-first-item">Our</span>
+            <span class="our-history__title-second-item">History</span>
+            <span class="our-history__title-description">It is a long established fact that a reader will be distracted by the readable content of a page when looking at.</span>
+            <span class="our-history__title-description">It is a long established fact that a reader will be distracted by the readable content of a page when looking at.</span>
+            <button type="button" class="btn btn-primary btn-lg main-button">Learn more</button>
+        </h2>
+
+    </div>
+
 </section>
